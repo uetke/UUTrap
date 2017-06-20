27d8cc14c32e6be9ba3a523e81f113735ccd1287
Yeroshenko, B. (Bohdan) <b.yeroshenko@uu.nl>
2017-06-20 10:28:17 +0200

    Update scanscript.py
    Script used for experiments. Double pass by line is implemented

--- Scripts/scanscript.py
+++ Scripts/scanscript.py
@@ -18,7 +18,6 @@ V0=5.0
 V1=5.0
 o0=Task()
 o1=Task()
-i2=Task()
 o0.CreateAOVoltageChan("/Dev1/ao0", "V0", 0.0, 10.0, DAQmx_Val_Volts, None)
 o1.CreateAOVoltageChan("/Dev1/ao1", "V1", 0.0, 10.0, DAQmx_Val_Volts, None)
 o0.WriteAnalogScalarF64(1,0,V0,None)
@@ -33,19 +32,15 @@ while key!=13 and key!=27:
         if key==72:
             print("Up")
             V0+=step
-            o0.WriteAnalogScalarF64(1, 0, V0, None)
         elif key==80:
             print("Down")
             V0-=step
-            o0.WriteAnalogScalarF64(1, 0, V0, None)
-        elif key==75:
-            print("Left")
-            V1+=step
-            o1.WriteAnalogScalarF64(1, 0, V1, None)
         elif key==77:
             print("Right")
+            V1+=step
+        elif key==75:
+            print("Left")
             V1-=step
-            o1.WriteAnalogScalarF64(1, 0, V1, None)
         elif key==73:
             print("No In yet")
         elif key==81:
@@ -62,40 +57,70 @@ while key!=13 and key!=27:
             key = ord(getch())
     else:
         print("Unrecognized input")
+    if V0 > 0 and V0 < 10 and V1 > 0 and V1 < 10:
+        o0.WriteAnalogScalarF64(1, 0, V1, None)
+        o1.WriteAnalogScalarF64(1, 0, V0, None)
+    else:
+        print("You tried leave allowed range")
     print("X Voltage: "+str(round(V0, 4))+"Y Voltage: "+str(round(V1, 4)))
     key = ord(getch())
 num=input("Name of file to write to: ")
 nm=num +".txt"
 dat = open(nm, 'w')
 itrt=int(input("Number of iterations: "))
-vol=int(input("What volt range do you want to scan? (<10): "))
+vol=float(input("What volt range do you want to scan? (<10): "))
 #Create array of values for output e.g. way of reference and voltages to apply
 Vo0=[V0-vol/2+vol*x/(itrt-1) for x in range(itrt)] #Array of voltages depending on iterations in limit [-10,10]
 Vo1=[V1-vol/2+vol*x/(itrt-1) for x in range(itrt)] #V0 - x for piezo, V1 - y
+Vo1r=Vo1[::-1]
 while min(Vo1)<0 or min(Vo0) <0 or max(Vo1)>10 or max(Vo0)>10:
     print(Vo0, Vo1)
     print("Chosen range cannot be used. Choose smaller range.")
-    vol = int(input("What volt range do you want to scan? (<10): "))
+    vol = float(input("What volt range do you want to scan? (<10): "))
     Vo0 = [V0 - vol / 2 + vol * x / (itrt - 1) for x in range(itrt)]  # Array of voltages depending on iterations in limit [-10,10]
     Vo1 = [V1 - vol / 2 + vol * x / (itrt - 1) for x in range(itrt)]
 print(Vo0, Vo1)
 read = int32()
 i2=Task()
 i2.CreateAIVoltageChan("Dev1/ai2", "", DAQmx_Val_RSE, -5.0, 5.0, DAQmx_Val_Volts, None)
-i2.CfgSampClkTiming("", 100000, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, 4000)
-data=np.zeros(4000)
-input("Find center of the focus and press Enter:")
+i2.CfgSampClkTiming("", 100000, DAQmx_Val_Rising, DAQmx_Val_FiniteSamps, 1000000)
+data=np.zeros(100)
+input("Start?")
+#input("Find center of the focus and press Enter:")
+i0=0
+j0=0
+forward=[]
+reverse=[]
 for i in Vo0:
-    o0.WriteAnalogScalarF64(1, 0, i, None)
+    o1.WriteAnalogScalarF64(1, 0, i, None)
     for j in Vo1:
-        o1.WriteAnalogScalarF64(1,0,j,None)
-        i2.ReadAnalogF64(4000, -1, DAQmx_Val_GroupByChannel, data, 4000, byref(read), None)
+        o0.WriteAnalogScalarF64(1,0,j,None)
+        i2.ReadAnalogF64(100, -1, DAQmx_Val_GroupByChannel, data, 100, byref(read), None)
         dat.write(str(i) + "\t" + str(j) + "\t" + str(np.average(data)) + "\n")
+        print("Point: "+str(i0)+":"+str(j0))
+        forward.append(j0)
+        j0+=1
+    for k in Vo1r:
+        o0.WriteAnalogScalarF64(1,0,j,None)
+        i2.ReadAnalogF64(100, -1, DAQmx_Val_GroupByChannel, data, 100, byref(read), None)
+        dat.write(str(i) + "\t" + str(j) + "\t" + str(np.average(data)) + "\n")
+        print("Point: "+str(i0)+":"+str(j0))
+        reverse.append(j0)
+        j0+=1
+    i0+=1
 dat.close()
 x,y,z= np.loadtxt(nm, delimiter='\t').T
+yf=[y[i] for i in forward]
+xf=[x[i] for i in forward]
+zb=[z[i] for i in forward]
+yb=[y[i] for i in reverse]
+xb=[x[i] for i in reverse]
+zf=[z[i] for i in reverse]
+za=[(zf[i]+zb[i])/2 for i in range(itrt*itrt)]
+zt=np.asarray(za)
 nrows=itrt
 ncols=itrt
-grid = z.reshape((nrows, ncols))
-plt.imshow(grid, extent=(x.min(), x.max(), y.max(), y.min()))
+grid = zt.reshape((nrows, ncols))
+plt.imshow(grid, extent=(min(xf), max(xf), max(yf), min(yf)))
 plt.colorbar()
 plt.show()
